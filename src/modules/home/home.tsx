e3a5003cb464e1bcc06352610b53967e24d94c8d
marksikaundi <emmanuelmark117@gmail.com>
2024-03-15 00:20:40 +0200

    Add react-fast-marquee component and Branding feature

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,11 +1,13 @@
 import Header from "@/shared/widgets/header/header";
 import Banner from "./features/Banner"
+import Branding from "./features/Branding";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Branding/>
     </div>
   );
 };
