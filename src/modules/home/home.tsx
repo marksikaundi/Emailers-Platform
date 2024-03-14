e6726622f494ebae1bebca293b38ae7e38800191
marksikaundi <emmanuelmark117@gmail.com>
2024-03-15 00:27:24 +0200

    Add Benefits feature to Home module

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,13 +1,15 @@
 import Header from "@/shared/widgets/header/header";
-import Banner from "./features/Banner"
+import Banner from "./features/Banner";
 import Branding from "./features/Branding";
+import Benefits from "./features/Benefits";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
-      <Branding/>
+      <Branding />
+      <Benefits />
     </div>
   );
 };
