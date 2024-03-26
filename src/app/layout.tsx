a8188dd40ad24c9d60cff6dda84dba9cad1ea291
marksikaundi <emmanuelmark117@gmail.com>
2024-03-26 22:35:25 +0200

    Refactor code for improved performance

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "../shared/styles/globals.css";
+import Providers from "@/shared/utils/Providers";
+import localFont from "next/font/local";
+import { ClerkProvider } from '@clerk/nextjs'
+
+const clashDisplay = localFont({
+  src: "../assets/fonts/ClashDisplay-Variable.ttf",
+  variable: "--font-clashDisplay",
+  weight: "700",
+});
+
+export const metadata: Metadata = {
+  title: "LupXml",
+  description: " Manage your emailers with ease and efficiency",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={`${clashDisplay.variable}`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
